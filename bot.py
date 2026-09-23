67605f5077d8a353efe91929ccdb919d89392da8
luaisgame <luaisgame@users.noreply.github.com>
2026-09-23 19:39:50 +0200

    Refresh launcher for JAR Java detection

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ load_dotenv(os.path.join(_BASE_DIR, ".env"))
 REPO = "luaisgame/decompiler"
 BRANCH = "main"
 RAW_URL = f"https://raw.githubusercontent.com/{REPO}/{BRANCH}"
-RAW_VERSION = "bc2d6e1"
+RAW_VERSION = "cbe5d19"
 
 GITHUB_FILES = [
     "commands/core.py",
