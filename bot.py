e3eb24efd7d1d3a04c27f5bd8b466d266833b453
luaisgame <luaisgame@users.noreply.github.com>
2026-09-23 19:46:19 +0200

    Refresh launcher for voice join fix

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ load_dotenv(os.path.join(_BASE_DIR, ".env"))
 REPO = "luaisgame/decompiler"
 BRANCH = "main"
 RAW_URL = f"https://raw.githubusercontent.com/{REPO}/{BRANCH}"
-RAW_VERSION = "cbe5d19"
+RAW_VERSION = "dd1249f"
 
 GITHUB_FILES = [
     "commands/core.py",
