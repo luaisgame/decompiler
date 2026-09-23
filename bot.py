c701df6cff446acaa00d0d923c781fbeaa925c0b
luaisgame <luaisgame@users.noreply.github.com>
2026-09-23 17:01:54 +0200

    Refresh launcher for Java compatibility

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ load_dotenv(os.path.join(_BASE_DIR, ".env"))
 REPO = "luaisgame/decompiler"
 BRANCH = "main"
 RAW_URL = f"https://raw.githubusercontent.com/{REPO}/{BRANCH}"
-RAW_VERSION = "2323686"
+RAW_VERSION = "3015481"
 
 GITHUB_FILES = [
     "commands/core.py",
